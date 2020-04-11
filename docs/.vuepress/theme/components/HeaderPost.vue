f2de2cbf2dd606166915cb626c3ffd3a232fbf0a
azertypow <azertypow@gmail.com>
2020-04-11 13:19:35 +0200

    feat: header image style

--- docs/.vuepress/theme/components/HeaderPost.vue
+++ docs/.vuepress/theme/components/HeaderPost.vue
@@ -1,7 +1,13 @@
 <template>
     <section class="v-header-post">
-        <img :src="imageURL"
-             v-for="imageURL of this.arrayOfImagesURL"
+        <img v-if="this.arrayOfImagesURL[0]"
+             :src="this.arrayOfImagesURL[0]"
+             class="v-header-post__img"
+             alt=""
+             :style="{transform: `translate3d(${randomPosition().x}px, ${randomPosition().y}px, ${randomPosition().z}px)`}"
+        >
+        <img v-for="imageURL of this.arrayOfImagesURL"
+             :src="imageURL"
              class="v-header-post__img"
              alt=""
              :style="{transform: `translate3d(${randomPosition().x}px, ${randomPosition().y}px, ${randomPosition().z}px)`}"
@@ -65,11 +71,35 @@
         display: block;
         position: absolute;
         top: -25%;
-        left: -25%;
-        width: calc( 100% / 2);
-        height: calc( 100% / 1.5);
+        left: 5%;
+        width: calc( 100% / 12);
+        height: calc( 100% / 2.5);
         object-fit: cover;
         object-position: top left;
+
+        &:nth-child(1),
+        &:nth-child(2) {
+            transform: none !important;
+            z-index: 10;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        &:nth-child(1) {
+            top: 0;
+            left: auto;
+            right: calc(100% / 12 * 10);
+            width: calc(100% / 12 * 2);
+            object-position: top right;
+        }
+
+        &:nth-child(2) {
+            top: 0;
+            right: auto;
+            left: calc(100% / 12 * 11);
+            width: calc(100% / 12 * 4);
+            object-position: bottom left;
+        }
     }
 
     // todo type
