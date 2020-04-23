bd418e453a57d1095f5180c204f8b0ad22e43e2f
azertypow <azertypow@gmail.com>
2020-04-23 20:03:05 +0200

    feat: add grid vue tool

---
+++ docs/.vuepress/theme/global-components/GridTool.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="v-grid-tool">
+        <div v-for="lineNumber in numberOfLines"
+             :style="{
+                top: padding_px * lineNumber + 'px'
+             }"
+             class="v-grid-tool__line"></div>
+    </section>
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "vue-property-decorator";
+
+    @Component({
+      mounted(this: GridTool) {
+        this.buildGrid()
+
+        window.addEventListener('resize', () => {
+          this.buildGrid()
+        })
+      }
+    })
+    export default class GridTool extends Vue {
+
+      @Prop({default: 10})
+      padding_px!: number
+
+      numberOfLines = 0
+
+      buildGrid() {
+
+        const gridContainerRect = this.$el.getBoundingClientRect()
+        this.numberOfLines = Math.floor( gridContainerRect.height / this.padding_px )
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .v-grid-tool {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        pointer-events: none;
+    }
+
+    .v-grid-tool__line {
+        position: absolute;
+        width: 100%;
+        height: 1px;
+        background: red;
+        opacity: .25;
+    }
+</style>
